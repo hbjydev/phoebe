84feb2b6568d54c00baf27808133d22c2d2d2f52
Hayden Young <hayden.young@zoodigital.com>
2025-07-23 10:19:22 +0100

    feat: authentik cors rule

--- terraform/modules/storage_bucket/main.tf
+++ terraform/modules/storage_bucket/main.tf
@@ -4,3 +4,19 @@ resource "cloudflare_r2_bucket" "self" {
   location = "WEUR"
   storage_class = var.storage_class
 }
+
+resource "cloudflare_r2_bucket_cors" "self" {
+  count = var.name == "sso-haydenmoe-storage" ? 1 : 0
+
+  account_id = var.account_id
+  bucket_name = cloudflare_r2_bucket.self.name
+  rules = [{
+    allowed = {
+      methods = ["GET"]
+      origins = ["https://sso.hayden.moe"]
+      headers = ["Authorization"]
+    }
+    id = "Allow Authentik access"
+    max_age_seconds = 3600
+  }]
+}
