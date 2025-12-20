b6de7eb322f21a9b81115ae429ec3a05b4f62f57
Hayden Young <hayden@hayden.moe>
2025-12-20 01:26:22 +0000

    feat: cf api token for cluster

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,7 +1,46 @@
+data "cloudflare_api_token_permission_groups_list" "self" {
+  provider = cloudflare.tokens
+}
+
+locals {
+  cf_api_perms = [
+    for x in data.cloudflare_api_token_permission_groups_list.self.result : {
+      id = x.id
+    }
+    if contains([
+      "Zone Read",
+      "DNS Read",
+      "DNS Edit",
+    ], x.name)
+  ]
+}
+
 resource "cloudflare_zone" "haydenmoe" {
   account = {
     id = var.CLOUDFLARE_ACCOUNT_ID
   }
 
   name = "hayden.moe"
+}
+
+resource "cloudflare_api_token" "self" {
+  name = "tok-phoebe-external-dns"
+
+  policies = [{
+    effect = "allow"
+    resources = jsonencode({
+      "com.cloudflare.api.account.zone.${cloudflare_zone.haydenmoe.id}" = "*"
+    })
+    permission_groups = local.cf_api_perms
+  }]
+
+  provider = cloudflare.tokens
+}
+
+resource "onepassword_item" "self" {
+  vault = var.PHOEBE_VAULT_ID
+  title = "svc-cloudflare"
+  tags = ["managed-by/terraform"]
+  category = "password"
+  password = cloudflare_api_token.self.value
 }
